efa34768561491c5b7330bb83213b793dc6e0483
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-02 11:09:31 +0100

    Add project files.

---
+++ Uppgift_7.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Uppgift_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> talLista = new List<int>()
+            {
+                14,
+                25,
+                100107,
+                49,
+            };
+
+            Console.WriteLine($"Det största talet i listan är {Max(talLista)}");
+        }
+        /// <summary>
+        /// Returnerar det största talet i en lista med int
+        /// </summary>
+        /// <param name="lista">Listan som ska sökas igenom</param>
+        /// <returns>int</returns>
+        static int Max(List<int> lista)
+        {
+            int maxTal = int.MinValue;
+
+            foreach (int i in lista)
+            {
+                if (i > maxTal)
+                {
+                    maxTal = i;
+                }
+            }
+
+            return maxTal;
+        }
+    }
+}
